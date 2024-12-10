eb775b12251646847dcf79db2c84df10af23b00d
pavelkalinchuk <715m4s@gmail.com>
2024-12-11 00:42:57 +0300

    Реализована проверка "test_product_buy_more_than_available"

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -33,7 +33,8 @@ class TestProducts:
     def test_product_buy_more_than_available(self, product):
         # TODO напишите проверки на метод buy,
         #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
-        pass
+        with pytest.raises(ValueError):
+            product.buy(1001)
 
 
 class TestCart:
